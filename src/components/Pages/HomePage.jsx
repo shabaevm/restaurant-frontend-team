3c3bdfc3668a459f8d10dfc28f2d2de951c0a4fc
SaydAliUsmanov <usmanov.guno@mail.ru>
2021-11-30 20:17:44 +0300

    collapsed components and added link for tables

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Tables from '../Tables';
+import Header from '../Header';
+import Products from '../Products';
 
 const HomePage = () => {
 	return (
 		<div className="bg-dark ">
+			<Header />
 			<Tables/>
+			<Products />
 		</div>
 	);
 };

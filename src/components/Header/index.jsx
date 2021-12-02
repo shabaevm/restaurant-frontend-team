6681f993514ed9549ddaaccea89c2a3ddcecdc5e
khamzat <khamz@mail.ru>
2021-12-02 11:47:35 +0300

    активные страницы

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import img_1 from '../../images/halal-logo.png'
-import cl from './header.module.css'
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import img_1 from "../../images/halal-logo.png";
+import cl from "./header.module.css";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-	const loading = useSelector(state => state.tables.loading)
-	return (
-		<>
-			{loading ? <div>Loading...</div> :
-				<div className={cl.nav}>
-					<div className={cl.wrapLogo}><img src={img_1} alt='' className={cl.logo}/></div>
-					<ul className={cl.menu}>
-						<li>
-							<NavLink className={cl.elementMenu} to='/about'>
-								О ресторане
-							</NavLink>
-						</li>
-						<li>
-							<NavLink className={cl.elementMenu} to="/contacts">
-								Контакты
-							</NavLink>
-						</li>
-						<li><i className={ `bi bi-box-arrow-in-right ${cl.imgRegistration}`}></i></li>
-					</ul>
-					
-				</div>
-			}
-		</>
-	)
+  const loading = useSelector((state) => state.tables.loading);
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className={cl.nav}>
+          <NavLink className={cl.wrapLogo} to="/">
+            <img src={img_1} alt="" className={cl.logo} />
+          </NavLink>
+          <ul className={cl.menu}>
+            <li>
+              <NavLink className={cl.elementMenu} to="/about">
+                О ресторане
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={cl.elementMenu} to="/contacts">
+                Контакты
+              </NavLink>
+            </li>
+            <li>
+              <i
+                className={`bi bi-box-arrow-in-right ${cl.imgRegistration}`}
+              ></i>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Header;
